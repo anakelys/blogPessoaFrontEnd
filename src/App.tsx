26530cd450d0d200a5f579fee357199988bc0511
anakelys <souzanakely@gmail.com>
2025-04-09 21:01:41 -0300

    Componente CardTemas e ListaTemas

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
+import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/temas/listatemas/ListaTemas'
 
 function App() {
     return (
         <>
-            <BrowserRouter>
+        <AuthProvider>
+        <BrowserRouter>
             <Navbar />
             <div className="min-h-[80vh]">
                 <Routes>
@@ -17,10 +20,13 @@ function App() {
                 <Route path="/home" element={<Home />} />
                 <Route path="/cadastro" element={<Cadastro />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/temas" element={<ListaTemas />} />
                 </Routes>
             </div>
             <Footer />
             </BrowserRouter>
+        </AuthProvider>
+
         </>
     )
 }
